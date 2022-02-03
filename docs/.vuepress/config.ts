ce4d0ad4dc753958a76b0a3fcdb0f0b141223ede
markthree <1801982702@qq.com>
2022-02-03 19:52:42 +0800

    feat: 添加navbar

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -9,7 +9,25 @@ export default defineUserConfig({
 	description: '基于 vuepress 的文档模板',
 	themeConfig: {
 		logo: '/images/logo.png',
-		logoDark: '/images/loadDarked.png'
+		logoDark: '/images/loadDarked.png',
+		navbar: [
+			{
+				text: '指南',
+				link: '/guide/'
+			},
+			{
+				text: '关于',
+				link: '/about/'
+			},
+			{
+				text: 'Gitee',
+				link: 'https://github.com/name/repo'
+			},
+			{
+				text: 'GitHub',
+				link: 'https://github.com/name/repo'
+			}
+		]
 	},
 	theme: resolve(__dirname, './theme/index.ts'),
 	bundlerConfig: {
