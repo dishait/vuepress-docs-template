dd42c46eaa7714c643b335e5208a720c29c5b807
markthree <1801982702@qq.com>
2022-02-03 19:06:42 +0800

    chore: 变更logo

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -7,7 +7,8 @@ export default defineUserConfig({
 	title: 'Name',
 	description: '基于 vuepress 的文档模板',
 	themeConfig: {
-		logo: 'https://vuejs.org/images/logo.png'
+		logo: '/images/logo.png',
+		logoDark: '/images/loadDarked.png'
 	},
 	theme: resolve(__dirname, './theme/index.ts'),
 	bundlerConfig: {
