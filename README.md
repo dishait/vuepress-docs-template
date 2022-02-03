dc428c23c9ab172cb55db4e8cf7910dc1befc9c0
markthree <1801982702@qq.com>
2022-02-03 14:56:14 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# docs-template
+
+基于 `vuepress` 的文档模板。 
---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,10 @@
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+	lang: 'zh-CN',
+	title: 'title',
+	description: '基于 vuepress 的文档模板',
+	themeConfig: {
+		logo: 'https://vuejs.org/images/logo.png'
+	}
+})
