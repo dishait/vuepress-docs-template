5ff0d1adc1ea6a2e2240a7e8f20a5b33f1ed1ba7
markthree <1801982702@qq.com>
2022-02-18 22:27:00 +0800

    chore: 添加主题主模块变更restart

--- docs/.vuepress/theme/index.ts
+++ docs/.vuepress/theme/index.ts
@@ -1,11 +1,22 @@
-import { path } from '@vuepress/utils'
+import { resolve } from 'path'
+import { watch } from 'chokidar'
 import { ThemeObject } from 'vuepress'
 
 const theme: ThemeObject = {
 	name: 'docs',
 	extends: '@vuepress/theme-default',
 	layouts: {
-		'404': path.resolve(__dirname, './layouts/404.vue')
+		'404': resolve(__dirname, './layouts/404.vue')
+	},
+	onWatched(app, watchers, restart) {
+		const themeWatcher = watch(
+			resolve(__dirname, './index.ts'),
+			{ ignoreInitial: true }
+		)
+
+		themeWatcher.once('change', restart)
+
+		watchers.push(themeWatcher)
 	}
 }
 
